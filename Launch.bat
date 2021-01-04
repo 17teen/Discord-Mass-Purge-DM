da24d36a0ba50138f7e5dfede6ff03f9c74c8ebc
7teen <71920969+17teen@users.noreply.github.com>
2021-01-04 10:05:54 +0000

    MASS PURGE DM's

--- Launch.bat
+++ Launch.bat
@@ -8,8 +8,6 @@ pause
     call npm install node-fetch
     call npm install @discordjs/uws@^10.149.0
     call npm install discord.js@11.5.0
-    call npm install random-puppy
-    call npm install figlet
     call npm install chalk
 pause
 cls
